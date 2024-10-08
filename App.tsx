246778bc72640b36c3112dcdbc8e7dd2cffa4204
Dev-Ericles <informaticaericles@gmail.com>
2024-10-07 21:58:16 -0300

    Trabalhando com Hook useState

--- App.tsx
+++ App.tsx
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  let [newTask, setNewTask] = React.useState('');
+
   return (
     <View style={styles.container}>
+      <Text>Trabalhando com Hook useState</Text>
+      <TextInput onChangeText={setNewTask} placeholder='Tarefa' style={styles.textinp}/>
+      <Text></Text>
+      <Text>Elemento digitado</Text>
+      <Text>{newTask}</Text>
+    {/*
       <Text>Digite seu nome</Text>
       <TextInput placeholder='Nome' style={styles.textinp}/>
+
+      <Text>Digite sua idade</Text>
+      <TextInput placeholder='Idade' style={styles.textinp}/>
+
+      <Text>Digite seu endereço</Text>
+      <TextInput placeholder='Endereço' style={styles.textinp}/>
       <TouchableOpacity activeOpacity={0.7} style={styles.button}>
-      <Text>Enviar</Text>
+        <Text>Adicionar tarefa</Text>
       </TouchableOpacity>
+        */}
       <StatusBar style="auto" />
     </View>
   );
